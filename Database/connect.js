40430807f92e891bebdef07d33ce24606c66cfef
Colack <UniformKitten281@gmail.com>
2024-12-02 09:43:04 -0800

    hide the server URL, move it to the .env file.

--- Database/connect.js
+++ Database/connect.js
@@ -2,7 +2,7 @@ const Mongoose = require("mongoose");
 const { Logger } = require('../util/logger');
 Mongoose.set('strictQuery',true);
 
-const uri ="mongodb+srv://dragonfire7z:"+process.env.MONGODB_PASSWORD+"@picmo.ti6ffzg.mongodb.net/?retryWrites=true&w=majority";
+const uri = process.env.MONGODB_PASSWORD;
 
 const connectDB = async () => {
   await Mongoose.connect(uri, {
