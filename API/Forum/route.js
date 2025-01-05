9c6d092574e3c9518ff0a0c37cd19061badf9971
DragonFireGames <107079704+DragonFireGames@users.noreply.github.com>
2025-01-05 08:58:36 +0100

    Update route.js

--- API/Forum/route.js
+++ API/Forum/route.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const Posts = require("../../Database/model/Posts")
 const PostAPI = require("./post");
 const poster = new PostAPI(Posts,"posts");
-const auths = require("../../Middleware/auth");
+const { userAuth, adminAuth, checkAuth } = require("../../Middleware/auth");
 
-poster.route(router,auths);
+poster.route(router,userAuth,adminAuth,checkAuth);
 
 module.exports = router;
