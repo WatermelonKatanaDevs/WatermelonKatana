99e64681785b337b7d34bd22815930cc7d9192cc
Varrience <105261606+Varrience@users.noreply.github.com>
2025-01-30 02:20:29 -0500

    only routes if submission is not processing

--- Middleware/auth.js
+++ Middleware/auth.js
@@ -64,9 +64,9 @@ exports.makeFormToken = function (req, res, next) {
 exports.checkFormToken = function(req, res, next) {
   let formToken = req.cookie("formToken");
   if(formToken && formTokens.get(formToken) !== "pending") {
-    formTokens.set(formToken, "processing")
+    formTokens.set(formToken, "processing");
+    next(()=>{res.clearCookie(); formTokens.delete(formToken)}, ()=>{formTokens.set(formToken, "pending")})
   } else {
     res.status(400).send("Form submission already in use");
   }
-  next(()=>{res.clearCookie(); formTokens.delete(formToken)}, ()=>{formTokens.set(formToken, "pending")})
 }
