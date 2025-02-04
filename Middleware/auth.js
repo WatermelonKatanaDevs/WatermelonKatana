470a43e0a5c0c5c5906496e50ad2c842cac80a76
Varrience <105261606+Varrience@users.noreply.github.com>
2025-02-03 22:49:45 -0500

    strengthen formTokens
    hopefully this should prevent token buildup as well for memory server side tokens not being used since some browsers don't retain cookies after the browser is closed

--- Middleware/auth.js
+++ Middleware/auth.js
@@ -52,24 +52,41 @@ exports.checkAuth = (req, res, next) => {
   }
 };
 
-exports.makeFormToken = function (req, res, next) {
-  let formToken = req.cookies["formToken"];
-  if (!formToken || !formTokens.get(formToken)) {
-    formToken = Math.random(0, Date.now()).toString(36).slice(2) + Math.random().toString(36).slice(2);
-    res.cookie("formToken", formToken);
-    formTokens.set(formToken, "pending");
+setInterval(function handleExpiry() {
+  let timestamp = Date.now();
+  for (let [tokenId, token] of formTokens) {
+    if (timestamp > token.expires) {
+      formTokens.delete(tokenId);
+    }
   }
+}, 6e4)
+
+exports.makeFormToken = function (req, res, next) {
+  let tokenId = req.cookies["formToken"];
+  let formToken = formTokens.get(tokenId);
+  if (formToken) { formTokens.delete(tokenId) }
+  generateFormToken(res);
   next();
 }
 
 exports.checkFormToken = function (req, res, next) {
-  let formToken = req.cookies["formToken"];
-  if (formToken && formTokens.get(formToken) === "pending") {
-    formTokens.set(formToken, "processing");
-    res.locals.clearCookie = () => { res.clearCookie("formToken"); formTokens.delete(formToken) }
-    res.locals.recycleCookie = () => { formTokens.set(formToken, "pending") }
+  let tokenId = req.cookies["formToken"];
+  let formToken = formTokens.get(tokenId);
+  if (tokenId && formToken.state === "pending") {
+    formToken.state = "processing";
+    res.locals.clearCookie = () => { res.clearCookie("formToken"); formTokens.delete(tokenId) }
+    res.locals.recycleCookie = () => { formToken.delete(tokenId); generateFormToken(res) }
     next();
   } else {
     res.status(400).send("Form submission already in use");
   }
+}
+
+function generateFormToken(res) {
+  tokenId = Math.random(0, Date.now()).toString(36).slice(2) + Math.random().toString(36).slice(2);
+  formTokens.set(tokenId, {
+    state: "pending",
+    expires: Date.now() + 9e5
+  });
+  res.cookie("formToken", tokenId);
 }
