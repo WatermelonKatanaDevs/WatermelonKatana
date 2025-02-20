b64f824bb42e2813a42d0ee6990b1d4bc0e90acd
Varrience <105261606+Varrience@users.noreply.github.com>
2025-02-20 06:35:34 -0500

    speedup censors?
    construct a list full of known censored words instead of constructing a search pattern for each specific word

--- util/js/censored.js
+++ util/js/censored.js
@@ -1,24 +1,25 @@
 const words = require('profane-words');
-const { LOG_LEVELS, logInfo, logWarn, logError, logDebug } = require('./logger');
+// const { LOG_LEVELS, logInfo, logWarn, logError, logDebug } = require('./logger');
+const pattern = new RegExp(`\\b${words.split("|")}\\b`, "gi");
 
 const censorText = (text) => {
-    let censoredText = text;
-    words.forEach((word) => {
-        const regex = new RegExp(`\\b${word}\\b`, 'gi');
-        censoredText = censoredText.replace(regex, '****');
-        //console.log(logWarn(`Censoring word: ${word}`));
-    });
+    let censoredText = text.replace(pattern, "****");
+    // words.forEach((word) => {
+    //     const regex = new RegExp(`\\b${word}\\b`, 'gi');
+    //     censoredText = censoredText.replace(regex, '****');
+    //     //console.log(logWarn(`Censoring word: ${word}`));
+    // });
     return censoredText;
 };
 
 const isProfane = (text) => {
-    for (let word of words) {
-        const regex = new RegExp(`\\b${word}\\b`, 'i');
-        if (regex.test(text)) {
-            return true;
-        }
-    }
-    return false;
+    // for (let word of words) {
+    //     const regex = new RegExp(`\\b${word}\\b`, 'i');
+    //     if (regex.test(text)) {
+    //         return true;
+    //     }
+    // }
+    return pattern.test(text);
 }
 
 module.exports = { isProfane, censorText };
