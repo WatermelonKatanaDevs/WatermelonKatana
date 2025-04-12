dcbccd48debd082d9aa6932ca63c63027ff2e125
Varrience <105261606+Varrience@users.noreply.github.com>
2025-04-12 16:37:42 -0400

    minor tweaks fix notif bar when dismissing

--- Assets/scripts/navbar.js
+++ Assets/scripts/navbar.js
@@ -387,8 +387,8 @@ async function notificationHTML(notif,index) {
     </div>
     <div style="display: flex">
       <span>${notif.content}<br>${new Date(notif.createdAt).toUTCString().replace(/\d\d:[^]+$/,"")}</span>
-      <button class="notifDismiss" onclick="event.preventDefault(); container = document.getElementById('notification-dropdown'), link = event.srcElement; while(!link.href) {link = link.parentElement} fetch(link.href); link.remove(); notifs = container.children; if(notifs.length<1){container.textContent='No Notifications'}else{for(i=0;i<notifs.length;i++){notifs[i].href='/notification/'+i}}">
-        <svg xmlns="http://www.w3.org/2000/svg" class="iconsvg" viewBox="0 0 640 512" style="width: 2em;"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M38.8 5.1C28.4-3.1 13.3-1.2 5.1 9.2S-1.2 34.7 9.2 42.9l592 464c10.4 8.2 25.5 6.3 33.7-4.1s6.3-25.5-4.1-33.7l-90.2-70.7c.2-.4 .4-.9 .6-1.3c5.2-11.5 3.1-25-5.3-34.4l-7.4-8.3C497.3 319.2 480 273.9 480 226.8l0-18.8c0-77.4-55-142-128-156.8L352 32c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 19.2c-42.6 8.6-79 34.2-102 69.3L38.8 5.1zM406.2 416L160 222.1l0 4.8c0 47-17.3 92.4-48.5 127.6l-7.4 8.3c-8.4 9.4-10.4 22.9-5.3 34.4S115.4 416 128 416l278.2 0zm-40.9 77.3c12-12 18.7-28.3 18.7-45.3l-64 0-64 0c0 17 6.7 33.3 18.7 45.3s28.3 18.7 45.3 18.7s33.3-6.7 45.3-18.7z"></path></svg>
+      <button class="notifDismiss" onclick="event.preventDefault(); container = document.getElementById('notification-dropdown'), link = event.srcElement.closest('.notifDismiss'); fetch(link.href); link.remove(); notifs = container.children; if(notifs.length<1){container.textContent='No Notifications'}else{for(i=0;i<notifs.length;i++){notifs[i].href='/notification/'+i}}" style="position: absolute;right: 1vmin;">
+        <svg xmlns="http://www.w3.org/2000/svg" class="iconsvg" viewBox="0 0 640 512" style="width: 2em;"><path d="M38.8 5.1C28.4-3.1 13.3-1.2 5.1 9.2S-1.2 34.7 9.2 42.9l592 464c10.4 8.2 25.5 6.3 33.7-4.1s6.3-25.5-4.1-33.7l-90.2-70.7c.2-.4 .4-.9 .6-1.3c5.2-11.5 3.1-25-5.3-34.4l-7.4-8.3C497.3 319.2 480 273.9 480 226.8l0-18.8c0-77.4-55-142-128-156.8L352 32c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 19.2c-42.6 8.6-79 34.2-102 69.3L38.8 5.1zM406.2 416L160 222.1l0 4.8c0 47-17.3 92.4-48.5 127.6l-7.4 8.3c-8.4 9.4-10.4 22.9-5.3 34.4S115.4 416 128 416l278.2 0zm-40.9 77.3c12-12 18.7-28.3 18.7-45.3l-64 0-64 0c0 17 6.7 33.3 18.7 45.3s28.3 18.7 45.3 18.7s33.3-6.7 45.3-18.7z"></path></svg>
       </button>
     </div>
   </a>`;
@@ -415,7 +415,7 @@ function openreportbtnclick() {
   var dropdown = document.querySelector("#report-dropdown");
   dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
 }
-// DROPDOWN KILLS ITSELF BECAUSE OF THIS SHIT, MAKE THIS BETTER BY USING FOCUS EVENTS SOON!
+
 document.addEventListener("DOMContentLoaded", ()=>{
   window.addEventListener("click", function(e) {
     const navbarRight = document.querySelector(".navbar-right");
@@ -423,7 +423,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
     const notificationDropdown = document.querySelector("#notification-dropdown");
     const reportIcon = document.querySelector("#report-icon");
     const reportDropdown = document.querySelector("#report-dropdown");
-    if(e.target === navbarRight) { return }
+    if(e.target === navbarRight || e.target.closest(".notifDismiss")) { return }
     if (notificationIcon && !notificationIcon.contains(e.target) && !notificationDropdown.contains(e.target)) {
       notificationDropdown.style.display = "none";
     }
