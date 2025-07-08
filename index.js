98c8720a283b10494ea416d3a11b1e95a1bd0d6d
Varrience <105261606+Varrience@users.noreply.github.com>
2025-07-07 20:27:43 -0400

    allow anon users to have limited dashboard

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ const cldir = __dirname + "/Pages";
  * Define route handlers for serving HTML files
  */
 app.get("/", (req, res) => res.sendFile(cldir + "/home.html")); // Home page
-app.get("/dashboard", userAuth, (req, res) => res.sendFile(cldir + "/dashboard.html")); // Dashboard page
+app.get("/dashboard", (req, res) => res.sendFile(cldir + "/dashboard.html")); // Dashboard page
 app.get("/register", makeFormToken, (req, res) => res.sendFile(cldir + "/users/auth/register.html")); // Registration page
 app.get("/login", (req, res) => res.sendFile(cldir + "/users/auth/login.html")); // Login page
 
