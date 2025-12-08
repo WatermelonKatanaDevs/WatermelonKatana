308dd0abde71e5e8fd2c1922e1ba9336aecd883e
Varrience <105261606+Varrience@users.noreply.github.com>
2025-12-08 00:12:49 -0500

    syntax prettify

--- util/js/censored.js
+++ util/js/censored.js
@@ -1,6 +1,6 @@
 const words = require('profane-words');
 const pattern = `\\b(${words.join("|")})\\b`;
-const censorPattern = new RegExp(pattern, 'gi');
+const censorPattern = new RegExp(pattern, "gi");
 const testPattern = new RegExp(pattern, "i");
 // const { LOG_LEVELS, logInfo, logWarn, logError, logDebug } = require('./logger');
 
