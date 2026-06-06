b779ede069248f0cdae47454aaff2085f9c38352
Varrience <105261606+Varrience@users.noreply.github.com>
2026-06-06 02:51:30 -0400

    functionality to view post content without js
    stuff that i do add like this will only have limited support to view content and will mainly be used by devs since the API path makes it easy to view heaps of data

--- API/Forum/post.js
+++ API/Forum/post.js
@@ -35,7 +35,7 @@ module.exports = class {
   notifyUserMentions(message, user, content, link) {
     return Promise.all([...new Set(message.match(/(?<=@)[^\s]+/g))].map(async function (name) {
       var mention = await Users.findOne({ username: name });
-      if(!mention) { return }
+      if (!mention) { return }
       mention.notify(user.username + " mentioned you!", content, link, user._id, user.username);
       await mention.save();
     }));
@@ -44,7 +44,7 @@ module.exports = class {
   notifyUserFollowers(title, user, content, link) {
     return Promise.all(user.followers.map(async function (fid) {
       var follower = await Users.findOne({ _id: fid });
-      if(!follower) { return }
+      if (!follower) { return }
       follower.notify(title, content, link, user._id, user.username);
       await follower.save();
     }));
@@ -178,7 +178,7 @@ module.exports = class {
 
   async list(req, res, next) {
     try {
-      const { poster, platform, postedBefore, postedAfter, includeTags, excludeTags, featured, randomEntryAction, page, total, sort, showMature, showHidden, showRecent, recipient, customQuery } = req.query;
+      const { poster, platform, postedBefore, postedAfter, includeTags, excludeTags, featured, randomEntryAction, page, total, sort, showMature, showHidden, showRecent, recipient, customQuery, noclient } = req.query;
       var search = { hidden: false };
       let uid = res.locals.userToken?.id;
       if (poster) search.poster = poster;
@@ -233,12 +233,46 @@ module.exports = class {
         list = await this.model.find(search);
       }
       list = list.map(e => e.pack());
+      list = await this.censor(list, res)
       var data = {
         [this.name]: list,
         length: length || list.length
       };
-      data = await this.censor(data, res);
-      if (randomEntryAction === 'redirect') {
+      if (interpretBool(noclient)) {
+        var page = "";
+        for (let entry of list) {
+          if (this.name === "posts") {
+            page += `<a class="post-panel" href="/forum/discussion/${entry.id}">
+              <div class="entry-top">
+                <h2>${entry.title}</h2> 
+                <p style="display: inline;">${entry.content}
+                <br>
+              By: <object><a href="/user/${entry.poster}"><i>${entry.poster}</i></a></object> | Views: ${entry.views} | Active ${entry.activeAt}</p>
+              <div class="forum-tags">${entry.tags.split(",")}</div>
+              </div>
+            </a>`
+          } else {
+            page += `<a class="project-panel" href="/project/${entry.id}">
+              <div class="thumbnail-border">
+                <div class="panel-overlay">
+                  <div>Score: ${entry.score} Views: ${entry.views}</div>
+                  <div>${entry.tags.split(",")}</div>
+                </div>
+                <img class="project-thumbnail" src="${entry.thumbnail || "/images/blank_project.png"}" alt="">
+              </div>
+              <div class="project-link">${entry.title}</div>
+              <div>By: <object><a href="/user/${entry.poster}"><i>${entry.poster}</i></a></object></div>
+            </a>`
+          }
+          page += `<details>
+          <summary> Comments: </comments>`
+          for(let comment of entry.comments) {
+            page += `<p> <b>${comment.poster}</b>: ${comment.content} <upvotes: ${comment.upvotes.length}>`
+          }
+          page += "</details>";
+        }
+        res.status(200).send(page);
+      } else if (randomEntryAction === 'redirect') {
         res.redirect("/project/" + data[this.name][0].id);
       } else {
         res.status(200).json(data);
@@ -269,11 +303,11 @@ module.exports = class {
         c.relevance = e.relevance;
         return c;
       });
+      list = await this.censor(list, res);
       var data = {
         [this.name]: list,
         length: length
       };
-      data = await this.censor(data, res);
       res.status(200).json(data);
     } catch (err) {
       res.status(401).json({ message: "Not successful", error: err.message });
